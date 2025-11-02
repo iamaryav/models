5c4fd9224536a9969412b97e69f61ef42ccba7c6
Priya Aryav <priya.aryav@gmail.com>
2025-11-02 22:17:50 +0530

    cuda

---
+++ kernels/hello.cu
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+__global__ void hello(){
+    printf("Hello, CUDA!\n");
+}
+
+int main(){
+    hello<<<1, 1>>>();
+    // Waits for kernel to finish
+    cudaDeviceSynchronize();
+    return 0;
+}
